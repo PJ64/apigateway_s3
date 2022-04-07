81e08d0301031d906b93568d92143db1248d6437
Paul Jackson <pjcksn@amazon.com>
2022-04-07 13:31:43 +1000

    Updated readme file

--- README.md
+++ README.md
@@ -1,14 +1,65 @@
-# Welcome to your CDK TypeScript project
+## Example
+This example demonstrates how to integrate Amazon API Gateway with Amazon S3 to perform basic S3 operations. The example uses the AWS CDK to deploy an Amazon S3 bucket and an Amazon API Gateway. The gateway is already configured with API methods that will list buckets, list objects inside a bucket, get an object from a bucket and add a new object to a bucket. Inside the Jupyter Notebook directory there is a client notebook with Python scripts for testing.
 
-This is a blank project for TypeScript development with CDK.
+![architecture](./images/architecture_1.png "Architecture")
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+## Setup
 
-## Useful commands
+You will need to download and install [Node.js](https://nodejs.org/en/download/) before you can start using the AWS Cloud Development Kit.
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+This example is developed using the AWS CDK and Typescript, so you will need to install both Typescript and the CDK using the following commands
+```
+npm install -g typescript
+npm install -g aws-cdk@latest
+```
+Since this CDK project uses ['Assests'](https://docs.aws.amazon.com/cdk/latest/guide/assets.html), you might need to run the following command to provision resources the AWS CDK will need to perform the deployment.
+
+```bash
+cdk bootstrap
+```
+
+The testing scripts can be executed using Jupyter Notebook. There are a few methods for installing Jupyter Notebooks. These instructions will help you get to started with [JupyterLab](https://jupyter.org/install) installation.
+
+You can also install Jupyter Notebooks as part of [Anaconda](https://docs.anaconda.com/anaconda/install/index.html) installation.
+
+To download this example, you will need to install [Git](https://github.com/git-guides/install-git). After installing git follow these [instructions](https://github.com/git-guides/git-clone) to learn how to clone the repository.
+
+After the repository has been cloned set the command prompt path to the cloned directory and run the following command to install the project dependencies.
+
+```bash
+npm install
+```
+
+**cdk synth** executes the application which translates the Typescript code into an AWS CloudFormation template.
+
+```bash
+cdk synth
+```
+
+After the synth command has generated the template use the  **cdk deploy** command to deploy the template to AWS CloudFormation and build the stack. You will be prompted to confirm the deployment with y/n.
+
+```bash
+cdk deploy
+```
+## Test the Stack
+We need to install Jest since we are using the Jest framework to test the stack. Testing the stack is optional.
+```
+npm install --save-dev jest @types/jest @aws-cdk/assert
+```
+
+## Run the Example
+Open the Jupyter Notebook in the **jupyter_notebook directory** follow the instructions.
+
+## Cleanup
+From the command prompt execute the following command: **cdk destroy**
+
+## Deployed Resources
+|	Identifier	|	Service	|	Type	|
+|	:---	|	:---	|	:---	|
+ApigwS3Stack-CustomS3AutoDeleteObjectsCustomResour-Sfisq5METC1E	|	Lambda	| Function
+ApigwS3Stack-CustomS3AutoDeleteObjectsCustomResour-ZMBDNMMX6314	|	IAM	| Role
+apigws3stack-s3bucket07682993-1n6xocsar8neq	|	S3	| Bucket
+restapis/vc64a34fx1	|	ApiGateway	| RestApi
+account/ApigwS-S3Obj-Q4xsvLTyC5jX	|	ApiGateway | Account
+ApigwS3Stack-S3ObjectsApiCloudWatchRole1AFD0F4C-1XRB3NL9ZQH2K	|	IAM	| Role
+ApigwS3Stack-apigatewayroleE6D48DBD-V4J987JKUHKO	|	IAM	| Role
--- lib/apigw_s3-stack.ts
+++ lib/apigw_s3-stack.ts
@@ -1,16 +1,190 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import { Role, ServicePrincipal, ManagedPolicy, PolicyStatement } from '@aws-cdk/aws-iam';
+import { Bucket } from '@aws-cdk/aws-s3';
+import { EndpointType,AuthorizationType,AwsIntegration, RestApi, PassthroughBehavior} from '@aws-cdk/aws-apigateway';
 
-export class ApigwS3Stack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class ApigwS3Stack extends cdk.Stack {
+  public readonly apiGatewayRole: Role;
+
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const s3_bucket = new Bucket(this, "S3Bucket" ,{
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApigwS3Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //Create REST API
+    const restApi = new RestApi(this, 'S3ObjectsApi', {
+      restApiName: 'Api Gateway S3 Proxy Service',
+      description: "S3 Actions Proxy API",
+      endpointConfiguration: {
+        types: [EndpointType.EDGE]
+      },
+      binaryMediaTypes: ['application/octet-stream', 'image/jpeg']
+    });
+
+    //Create {folder} API resource to list objects in a given bucket
+    const bucketResource = restApi.root.addResource("{folder}");
+
+    //Create {object} API resource to read/write an object in a given bucket
+    const bucketItemResource = bucketResource.addResource("{object}");
+
+    // Create IAM Role for API Gateway
+    this.apiGatewayRole = new Role(this, 'api-gateway-role', {
+      assumedBy: new ServicePrincipal('apigateway.amazonaws.com')
+    });
+
+    //ListAllMyBuckets method
+    this.addActionToPolicy("s3:ListAllMyBuckets");
+    const listMyBucketsIntegration = new AwsIntegration({
+      service: "s3",
+      region: this.region,
+      path: '/',
+      integrationHttpMethod: "GET",
+      options: {
+        credentialsRole: this.apiGatewayRole,
+        passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
+        integrationResponses: [{
+          statusCode: '200',
+          responseParameters: { 'method.response.header.Content-Type': 'integration.response.header.Content-Type' }
+        }]
+      }
+    });
+    //ListAllMyBuckets method options
+    const listMyBucketsMethodOptions = {
+      authorizationType: AuthorizationType.NONE,
+      methodResponses: [
+        {
+          statusCode: '200',
+          responseParameters: {
+            'method.response.header.Content-Type': true
+          }
+        }]
+    };
+    restApi.root.addMethod("GET", listMyBucketsIntegration, listMyBucketsMethodOptions);
+
+    //ListBucket (Objects) method
+    this.addActionToPolicy("s3:ListBucket");
+    const listBucketIntegration = new AwsIntegration({
+      service: "s3",
+      region: this.region,
+      path: '{bucket}',
+      integrationHttpMethod: "GET",
+      options: {
+        credentialsRole: this.apiGatewayRole,
+        passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
+        requestParameters: { 'integration.request.path.bucket': 'method.request.path.folder' },
+        integrationResponses: [{
+          statusCode: '200',
+          responseParameters: { 'method.response.header.Content-Type': 'integration.response.header.Content-Type' }
+        }]
+      }
+    });
+
+    //ListBucket (Objects) method options
+    const listBucketMethodOptions = {
+      authorizationType: AuthorizationType.NONE,
+      requestParameters: {
+        'method.request.path.folder': true
+      },
+      methodResponses: [
+        {
+          statusCode: '200',
+          responseParameters: {
+            'method.response.header.Content-Type': true
+          }
+        }]
+    };
+    bucketResource.addMethod("GET", listBucketIntegration, listBucketMethodOptions);
+
+    //GetObject method
+    this.addActionToPolicy("s3:GetObject");
+    const getObjectIntegration = new AwsIntegration({
+      service: "s3",
+      region: this.region,
+      path: '{bucket}/{object}',
+      integrationHttpMethod: "GET",
+      options: {
+        credentialsRole: this.apiGatewayRole,
+        passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
+        requestParameters: {
+          'integration.request.path.bucket': 'method.request.path.folder',
+          'integration.request.path.object': 'method.request.path.object',
+          'integration.request.header.Accept': 'method.request.header.Accept'
+        },
+        integrationResponses: [{
+          statusCode: '200',
+          responseParameters: { 'method.response.header.Content-Type': 'integration.response.header.Content-Type' }
+        }]
+      }
+    });
+
+    //GetObject method options
+    const getObjectMethodOptions = {
+      authorizationType: AuthorizationType.NONE,
+      requestParameters: {
+        'method.request.path.folder': true,
+        'method.request.path.object': true,
+        'method.request.header.Accept': true
+      },
+      methodResponses: [
+        {
+          statusCode: '200',
+          responseParameters: {
+            'method.response.header.Content-Type': true
+          }
+        }]
+    };
+    bucketItemResource.addMethod("GET", getObjectIntegration, getObjectMethodOptions);
+
+    //PutObject method
+    this.addActionToPolicy("s3:PutObject");
+    const putObjectIntegration = new AwsIntegration({
+      service: "s3",
+      region: this.region,
+      path: '{bucket}/{object}',
+      integrationHttpMethod: "PUT",
+      options: {
+        credentialsRole: this.apiGatewayRole,
+        passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
+        requestParameters: {
+          'integration.request.path.bucket': 'method.request.path.folder',
+          'integration.request.path.object': 'method.request.path.object',
+          'integration.request.header.Accept': 'method.request.header.Accept'
+        },
+        integrationResponses: [{
+          statusCode: '200',
+          responseParameters: { 'method.response.header.Content-Type': 'integration.response.header.Content-Type' }
+        }]
+      }
+    });
+
+    //PutObject method options
+    const putObjectMethodOptions = {
+      authorizationType: AuthorizationType.NONE,
+      requestParameters: {
+        'method.request.path.folder': true,
+        'method.request.path.object': true,
+        'method.request.header.Accept': true,
+        'method.request.header.Content-Type': true
+      },
+      methodResponses: [
+        {
+          statusCode: '200',
+          responseParameters: {
+            'method.response.header.Content-Type': true
+          }
+        }]
+    };
+    bucketItemResource.addMethod("PUT", putObjectIntegration, putObjectMethodOptions);
+  }
+  private addActionToPolicy(action: string) {
+    this.apiGatewayRole.addToPolicy(new PolicyStatement({
+      resources: [
+        "*"
+      ],
+      actions: [`${action}`]
+    }));
   }
 }
